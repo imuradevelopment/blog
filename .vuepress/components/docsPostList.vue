28e6423f887bb5d696a1317ec054f9092b873623
account0248 <miyaura@vaile.co.jp>
2022-03-27 17:23:58 +0900

    記事一覧のパス修正

--- .vuepress/components/docsPostList.vue
+++ .vuepress/components/docsPostList.vue
@@ -2,9 +2,9 @@
 <template>
     <div>
         <div style="margin-bottom:1.5rem" v-for="post in posts" v-bind:key="post.id">
-            <h2 style="margin-bottom:0"><a v-bind:href="post.path">{{post.title}}</a></h2>
+            <h2 style="margin-bottom:0"><a v-bind:href="base + post.path">{{post.title}}</a></h2>
             <p style="margin:0 0 0 0.5rem; line-height: 1.5em;">{{post.frontmatter.description}}</p>
-            <a style="display:inline-block; margin-top:0.5rem;" v-bind:href="post.path">続きを読む</a>
+            <a style="display:inline-block; margin-top:0.5rem;" v-bind:href="base + post.path">続きを読む</a>
         </div>
     </div>
 </template>
@@ -14,9 +14,18 @@ export default {
     rootDir: String
   },
   computed: {
+    base(){
+      let base = this.$site.base
+      const l = base.length
+      if(base.substring(l-1,l) == '/'){
+        return base.substr(0, l-1)
+      }else{
+        return base
+      }
+    },
     posts() {
       return (this.$site.pages)
-        // blogディレクトリ以下を投稿記事一覧表示の対象とする
+        // docsディレクトリ以下を投稿記事一覧表示の対象とする
         .filter(post => post.path.startsWith('/docs/'))
         .filter(post => post.title != "記事一覧")
         // dateに設定した日付の降順にソートする
